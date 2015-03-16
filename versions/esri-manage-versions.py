618c74c79ca30280ebbd9381a47b20ad6adf1701
sde <sde@coa-gis-jobs1.asheville.local>
2015-03-16 11:55:07 -0400

    added delete,compress,create -needs cleanup

--- versions/esri-manage-versions.py
+++ versions/esri-manage-versions.py
@@ -5,6 +5,26 @@ import os
 from arcpy import env  
 import yaml
 
+
+#create version
+def createver(key,workspace):
+  for k in key:
+   for key in k: 
+     if 'version-name' in key:
+       
+       #Execute CreateVersion
+       arcpy.CreateVersion_management(workspace, k['parent-version'], k['version-name'], k['access-permission']) 
+       
+       #Alter Version
+       arcpy.AlterVersion_management(workspace,  k['version-name'],  k['version-name'], 'version for: '+ k['version-name'], k['access-permission'])
+
+#delete version
+def deletever(key,workspace):
+  for k in key:
+   for key in k:
+     if 'version-name' in key:
+       arcpy.DeleteVersion_management(workspace, k['version-name'],) 
+
 #delete sde connections 
 def deleteconn(configkey):
   if configkey['out_folder_path'] is None:
@@ -36,10 +56,29 @@ with open("config/config.yaml", 'r') as ymlfile:
 for key, value in cfg.items():
   connections =  cfg[key]
   for k in connections:
-    connsde(k) 
-    if k['out_folder_path'] is not None:
-      versions = arcpy.ListVersions(k['out_folder_path']+k['out_name'])
-    else:
-      versions = arcpy.ListVersions(k['out_name'])
-    print(versions)
+    connsde(k)
 
+  #delete
+  for k in connections:
+    if  'versions' in k:
+      ver = k['versions']
+      if k['out_folder_path'] is not None:
+        deletever(ver,k['out_folder_path']+k['out_name'])
+      else:
+        deletever(ver,k['out_name'])
+ 
+  #compress
+  for k in connections:
+      if k['out_folder_path'] is not None:
+        arcpy.Compress_management(k['out_folder_path']+k['out_name'])
+      else:
+        arcpy.Compress_management(k['out_name'])
+  
+  #create
+  for k in connections:
+    if  'versions' in k:
+      ver = k['versions']
+      if k['out_folder_path'] is not None:
+        createver(ver,k['out_folder_path']+k['out_name'])
+      else:
+        createver(ver,k['out_name']) 
