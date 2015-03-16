7bce3fbf10362874db1f9a4ac5bca89a63806b2f
sde <sde@coa-gis-jobs1.asheville.local>
2015-03-16 16:17:33 -0400

    added email logging

--- versions/test.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-import logging,logging.handlers 
-smtpHandler = logging.handlers.SMTPHandler(mailhost=("192.168.0.102",25), fromaddr="dmichelson@ashevillenc.gov", toaddrs="dmichelson@ashevillenc.gov", subject=u"error message") 
-
-LOG = logging.getLogger() 
-LOG.addHandler(smtpHandler) 
-
-LOG.error(u"tes1") 
-LOG.error(u"test2") 
